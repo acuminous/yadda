1f5788d6443291c72641adc185e07594e04843a5
Stephen Cresswell <atlassian@stephen-cresswell.net>
2013-01-23 07:09:50 +0000

    Improved mocha example

--- examples/mocha/test.js
+++ examples/mocha/test.js
@@ -7,17 +7,22 @@ var yadda = new Yadda.yadda(library, assert);
 
 describe('100 Green Bottles', function() {
     describe('A bottle falls from the wall', function() {
-        it('Should leave 99 bottles', function() {
+        it('Should leave 99 bottles on the wall', function() {
             yadda.yadda([
                 "Given 100 green bottles are standing on the wall",
                 "when 1 green bottle accidentally falls",
                 "then there are 99 green bottles standing on the wall",
-
-                "when another green bottle accidentally falls",
+            ]);
+        })
+    })
+    describe('A bottle bounces back after falling', function() {
+        it('Should leave 100 bottles on the wall', function() {
+            yadda.yadda([
+                "When another green bottle accidentally falls",
                 "with a loud bang",
                 "but bounces back",
                 "then there are still 99 green bottles standing on the wall"
-            ]);
+            ])
         })
     })
 })
