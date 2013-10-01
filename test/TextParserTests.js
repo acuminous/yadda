b7080b464ebae3ec882274d2a4e12b433af83e92
Colin Vipurs <colin.vipurs@shazamteam.com>
2013-09-30 17:42:02 -0700

    https://github.com/acuminous/yadda/issues/21 Added in ability to specify annotations in feature files.  For now they are just ignored

--- test/TextParserTests.js
+++ test/TextParserTests.js
@@ -7,6 +7,7 @@ describe('TextPaser', function() {
     var simple_feature = ['Feature: Tests with feature', 'Scenario: With Feature'].join('\n');
     var multiple_feature = ['Feature: Tests with 2 features', 'Scenario: For Feature 1', 'Feature: Second feature'].join('\n');
     var complex_scenario = ['Scenario: Complex', '', '  ', '   Given A', '', 'When B', ' ', '   Then C'].join('\n');
+    var annotated_scenario = ['@keyword=value', 'Scenario: Annotated', 'Given A', 'When B', 'Then C'].join('\n');
     var parser;
 
     beforeEach(function(){
@@ -48,5 +49,12 @@ describe('TextPaser', function() {
         assert.throws(function() {
             parser.parse(multiple_feature);
         }, /single feature/);
+    });
+
+    it('should ignore annotations', function() {
+        var scenarios = parser.parse(annotated_scenario).scenarios;
+        assert.equal(scenarios.length, 1);
+        assert.equal(scenarios[0].title, 'Annotated');
+        assert.deepEqual(scenarios[0].steps, ['Given A', 'When B', 'Then C']);
     });
 });
