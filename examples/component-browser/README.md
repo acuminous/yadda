694dc330016b97536d65386852ca598329761c77
John Syrinek <john.syrinek@gmail.com>
2014-01-21 19:12:23 -0600

    Adds support for component.io (in the browser)

---
+++ examples/component-browser/README.md
@@ -0,0 +1,7 @@
+```
+npm install
+npm install -g http-server
+make
+http-server &
+open http://localhost:8080/index.htm
+```
---
+++ examples/component-browser/test.js
@@ -0,0 +1,19 @@
+(function() {
+    var Yadda = require('yadda'),
+        English = Yadda.localisation.English,
+        FeatureParser = Yadda.parsers.FeatureParser,
+        parser = new FeatureParser(English),
+        bottles = require('yadda-component-browser-example/features/bottles.feature.js'),
+        steps = require('yadda-component-browser-example/bottles-library.js'),
+        steps = new Yadda.Yadda(steps),
+        steps = steps.yadda.bind(steps);
+
+    mocha.setup('bdd');
+    Yadda.plugins.mocha();
+
+    feature(bottles, function (feature) {
+        scenarios(feature.scenarios, function(scenario, done) {
+            steps(scenario.steps, done);
+        });
+    });
+})();
