bbade115ffd2488b1f20001da64f1281a0560121
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-07-13 20:04:08 +0100

    lint

--- examples/jasmine/spec/bottles-spec.js
+++ examples/jasmine/spec/bottles-spec.js
@@ -1,5 +1,5 @@
 /* jslint node: true */
-/* global featureFile, scenarios, steps */
+/* global featureFile, scenarios, steps, jasmine */
 "use strict";
 
 var SpecReporter = require('jasmine-spec-reporter');
