f8b1acecc2c745a8313d69f21cf066aea6b513af
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-07-24 10:39:01 +0100

    Adding list converter

---
+++ test/converters/ListConverterTest.js
@@ -0,0 +1,17 @@
+/* jslint node: true */
+/* global describe, it */
+"use strict";
+
+var assert = require('assert');
+var convert = require('../../lib/converters/list-converter');
+
+describe('List Converter', function() {
+
+    it("Should convert strings to lists", function(next) {
+        convert("a\nb\nc", function(err, value) {
+            assert.ifError(err);
+            assert.deepEqual(value, ['a', 'b', 'c']);
+            next();
+        });
+    });
+});
