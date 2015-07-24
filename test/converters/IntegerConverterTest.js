a62b075d11e404168d24fd78e5849e0fc70dfdd1
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-07-24 10:20:00 +0100

    Integer converter yields errors

---
+++ test/converters/IntegerConverterTest.js
@@ -0,0 +1,27 @@
+/* jslint node: true */
+/* global describe, it */
+"use strict";
+
+var assert = require('assert');
+var convert = require('../../lib/converters/integer-converter');
+
+describe('Integer Converter', function() {
+
+    it("Should convert strings to integers", function(next) {
+        convert("1", function(err, value) {
+            assert.ifError(err);
+            assert.equal(value, 1);
+            assert.equal(typeof value, 'number');
+            next();
+        })
+    });
+
+    it("Should error on NaN", function(next) {
+        convert("a", function(err, value) {
+            assert(err);
+            assert.equal(err.message, 'Cannot convert [a] to an integer');
+            next();
+        })
+    });
+
+});
