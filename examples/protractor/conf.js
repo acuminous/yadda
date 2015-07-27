e0d3debb5909dbd59a012bf3ee682ec4d5d8c298
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-07-27 23:30:53 +0100

    Using spec reporter

--- examples/protractor/conf.js
+++ examples/protractor/conf.js
@@ -8,6 +8,11 @@ exports.config = {
         isVerbose: false,
         showColors: true,
         includeStackTrace: true,
-        defaultTimeoutInterval: 30000
+        defaultTimeoutInterval: 30000,
+       print: function() {}
+    },
+    onPrepare: function() {
+      var SpecReporter = require('jasmine-spec-reporter');
+      jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
     }
 }
