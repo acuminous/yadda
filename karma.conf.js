878798baf365f367d19c6e15ab5f417da709e0b8
inf3rno <i.dont.give.my@email.addr>
2016-03-11 03:16:51 +0100

    Added Karma support with Browserify. #58 Removed env variables from npm scripts. #198

---
+++ karma.conf.js
@@ -0,0 +1,42 @@
+module.exports = function (config) {
+    config.set({
+        plugins: [
+            "karma-browserify",
+            "karma-phantomjs-launcher",
+            "karma-mocha"
+        ],
+        frameworks: ["browserify", "mocha"],
+        files: [
+            "lib/**/*.js",
+            {pattern: "lib/**/!(*.js)", included: false},
+            "test/**/*.js",
+            {pattern: "test/**/!(*.js)", included: false}
+        ],
+        preprocessors: {
+            "lib/**/*.js": ["browserify"],
+            "test/**/*.js": ["browserify"]
+        },
+        client: {
+            mocha: {
+                reporter: "html",
+                ui: "bdd"
+            }
+        },
+        browserify: {
+            debug: true,
+            configure: function (bundle) {
+                bundle.on('prebundle', function () {
+                    bundle.require("./lib/index", {expose: "yadda"});
+                });
+            }
+        },
+        browsers: ["PhantomJS"],
+        reporters: ["progress"],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        captureTimeout: 6000,
+        singleRun: true
+    });
+};
---
+++ test/shims/FileSystemTests.js
@@ -0,0 +1,42 @@
+/* jslint node: false */
+/* global describe, it */
+"use strict";
+
+var assert = require("assert"),
+    fs = require("../../lib/shims").fs,
+    path = require("../../lib/shims").path;
+
+describe("FileSystem", function () {
+
+    it("should check path existence", function () {
+
+        assert(fs.existsSync("./test/shims/data/subdir1"));
+        assert(fs.existsSync("./test/shims/data/file3.json"));
+        assert(fs.existsSync("test/shims/data/subdir2"));
+        assert(fs.existsSync("test/shims/data/subdir2/file2.feature"));
+        assert(fs.existsSync(path.join(__dirname, "data/subdir1/file1.json")));
+        assert(!fs.existsSync("./test/shims/data/non.existent"));
+    });
+
+    it("should distinguish files and directories", function () {
+
+        var dirStat = fs.statSync("./test/shims/data/subdir1");
+        assert(dirStat.isDirectory());
+        var fileStat = fs.statSync("./test/shims/data/subdir2/file2.feature");
+        assert(!fileStat.isDirectory());
+    });
+
+    it("should get directory content for a single level depth", function () {
+
+        var content = fs.readdirSync("./test/shims/data");
+        assert.deepEqual(content.sort(), ["subdir1", "subdir2", "file3.json"].sort());
+    });
+
+    it("should get file content as string", function () {
+
+        var content = fs.readFileSync("./test/shims/data/subdir1/file1.json", "utf8");
+        var json = JSON.parse(content);
+        assert.deepEqual(json, {x: 1, y: 2});
+    });
+
+});
