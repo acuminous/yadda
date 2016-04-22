a83f4ae341dc21a6883e5951331ee3670b333807
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-04-22 09:31:53 +0100

    Failing to demonstrate #211

--- examples/mocha-async/bottles-library.js
+++ examples/mocha-async/bottles-library.js
@@ -13,23 +13,32 @@ module.exports = (function() {
     var library = English.library(dictionary)
 
     .given("a $NUM foot wall", function(height, next) {
-        wall = new Wall();
-        next();
+        setTimeout(function() {
+            wall = new Wall();
+            next();
+        });
     })
 
     .given("$NUM green bottles are standing on the wall", function(number_of_bottles, next) {
-        wall.bottles = number_of_bottles;
-        next();
+        setTimeout(function() {
+            wall.bottles = number_of_bottles;
+            next();
+        });
     })
 
     .when("$NUM green bottle accidentally falls", function(number_of_falling_bottles, next) {
-        wall.fall(number_of_falling_bottles);
-        next();
+        setTimeout(function() {
+            wall.fall(number_of_falling_bottles);
+            next();
+        });
     })
 
     .then("there (?:are|are still) $NUM green bottles standing on the wall", function(number_of_bottles, next) {
-        assert.equal(number_of_bottles, wall.bottles);
-        next();
+        setTimeout(function() {
+            throw new Error('foo')
+            assert.equal(number_of_bottles, wall.bottles);
+            next();
+        });
     });
 
     var Wall = function(bottles) {
